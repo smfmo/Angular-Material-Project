5d71d47b176a7f1eb5b025c5becf6bbbf7a20b9b
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-01 17:45:13 -0300

    Implementação das rotas da aplicação (Cadastro e consulta)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+/*
+-> Aonde eu coloco as rotas da aplicação
+*/
+export const routes: Routes = [
+    { path:'cadastro', component: CadastroComponent },
+    { path:'consulta', component: ConsultaComponent}
+];
